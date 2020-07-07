8479285978513c8ccb049180aa5e784c12d9f7d7
PickledFish <suancaiyu0413@gmail.com>
2020-07-07 09:32:25 +0800

    修改模型，title字段改为json类型

--- model/ams_security.go
+++ model/ams_security.go
@@ -2,7 +2,6 @@ package model
 
 import (
 	"Ams/config"
-	"fmt"
 	"strings"
 )
 
@@ -11,19 +10,18 @@ type Domains struct {
 	ID     int    `gorm:"primary_key;column:id;type:int(11) unsigned;not null"`
 	Domain string `gorm:"unique;column:domain;type:varchar(120);not null"`
 	DNS    string `gorm:"column:dns;type:json"`
-	Title  string `gorm:"column:title;type:varchar(255)"`
+	Title  string `gorm:"column:title;type:json"`
 	Status int    `gorm:"column:status;type:int(11) unsigned"`
 	Fid    int    `gorm:"index;column:fid;type:int(11) unsigned;not null"`
 }
 
 func AddDomainRow(domain string, fid int) *Domains {
 	db := GetAppDB(*config.LoadConfig())
-	row := &Domains{Domain: domain, Fid: fid,DNS: "[]"}
+	row := &Domains{Domain: domain, Fid: fid,DNS: "[]",Title: "[]"}
 	if err := 	db.Create(row).Error;err != nil{
 		if strings.Contains(err.Error()," Duplicate entry"){
 			var d = &Domains{}
 			db.Where("domain = ?",row.Domain).Find(&d)
-			fmt.Println("-----",d.ID,d.Domain)
 			return d
 		}
 	}
